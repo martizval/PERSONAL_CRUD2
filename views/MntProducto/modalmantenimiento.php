57384f6a3178fc751a670d73f27c7f8b85ef35f2
martizval <N1tr4mxz+>
2021-06-08 02:06:48 -0500

    Subiendo a GitHub 8/06/2021

---
+++ views/MntProducto/modalmantenimiento.php
@@ -0,0 +1,13 @@
+<div id="modalmantenimiento" class="modal fade bd-example-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">
+					<i class="font-icon-close-2"></i>
+				</button>
+				<h4 class="modal-title" id="mdltitulo"></h4>
+				<form method="post" id="producto_form"></form>
+			</div>
+		</div>	
+	</div>
+</div>
